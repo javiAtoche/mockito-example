ec378bc567271f59daf3eb6045f5c16dde43e090
javiAtoche <javieratocheortega@gmail.com>
2019-06-03 21:12:23 +0200

    mockito en casa - lunes 03 Junio 2019

---
+++ src/test/java/com/in28minutes/mockito/TestMockito1.java
@@ -0,0 +1,14 @@
+package com.in28minutes.mockito;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMockito1 {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
